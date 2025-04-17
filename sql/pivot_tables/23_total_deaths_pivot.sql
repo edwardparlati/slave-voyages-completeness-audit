a25392623e2d89996258462a5a306a3cfcc52996
edwardparlati <edward.parlati@gmail.com>
2025-04-17 10:39:23 -0400

    Add files via upload

---
+++ sql/pivot_tables/23_total_deaths_pivot.sql
@@ -0,0 +1,57 @@
+-- Total Deaths Completeness by Decade with Broad Regions as Columns
+WITH disembark_regions AS (
+  SELECT
+    i.voyage_id,
+    FLOOR(d.voyage_start_imputed_year / 10) * 10 AS decade,
+    CAST(SUBSTR(CAST(i.primary_disembark_port AS STRING), 1, 1) AS INT64) * 10000 AS broad_region_code
+  FROM `slave_voyages.voyages_itinerary` i
+  JOIN `slave_voyages.voyages_dates` d ON i.voyage_id = d.voyage_id
+  WHERE i.primary_disembark_port IS NOT NULL
+),
+labeled_regions AS (
+  SELECT DISTINCT broad_region_value, broad_region
+  FROM `slave_voyages.lookup_voyages_geographic_codes`
+),
+distinct_decades AS (
+  SELECT DISTINCT FLOOR(voyage_start_imputed_year / 10) * 10 AS decade
+  FROM `slave_voyages.voyages_dates`
+),
+all_combinations AS (
+  SELECT d.decade, r.broad_region
+  FROM distinct_decades d
+  CROSS JOIN (SELECT DISTINCT broad_region FROM labeled_regions) r
+),
+percent_complete AS (
+  SELECT
+    r.broad_region,
+    i.decade,
+    COUNT(c.slave_death_total_derived) / COUNT(*) * 100 AS completeness_pct
+  FROM `slave_voyages.voyages_slave_characteristics` c
+  JOIN disembark_regions i ON c.voyage_id = i.voyage_id
+  JOIN labeled_regions r ON i.broad_region_code = r.broad_region_value
+  GROUP BY r.broad_region, i.decade
+),
+filled_data AS (
+  SELECT
+    a.decade,
+    a.broad_region,
+    IFNULL(p.completeness_pct, 0) AS completeness_pct
+  FROM all_combinations a
+  LEFT JOIN percent_complete p ON a.decade = p.decade AND a.broad_region = p.broad_region
+)
+SELECT * FROM filled_data
+PIVOT (
+  MAX(completeness_pct)
+  FOR broad_region IN (
+    'Europe',
+    'Mainland North America',
+    'Caribbean',
+    'Spanish Mainland Americas',
+    'Brazil',
+    'Africa',
+    'Middle East',
+    'Asia',
+    'Other'
+  )
+)
+ORDER BY decade;
---
+++ sql/pivot_tables/30_pivot_data.sql
@@ -0,0 +1,521 @@
+-- Total Embarkation Completeness by Decade with Broad Regions as Columns
+WITH disembark_regions AS (
+  SELECT
+    i.voyage_id,
+    FLOOR(d.voyage_start_imputed_year / 10) * 10 AS decade,
+    CAST(SUBSTR(CAST(i.primary_disembark_port AS STRING), 1, 1) AS INT64) * 10000 AS broad_region_code
+  FROM `slave_voyages.voyages_itinerary` i
+  JOIN `slave_voyages.voyages_dates` d ON i.voyage_id = d.voyage_id
+  WHERE i.primary_disembark_port IS NOT NULL
+),
+labeled_regions AS (
+  SELECT DISTINCT broad_region_value, broad_region
+  FROM `slave_voyages.lookup_voyages_geographic_codes`
+),
+distinct_decades AS (
+  SELECT DISTINCT FLOOR(voyage_start_imputed_year / 10) * 10 AS decade
+  FROM `slave_voyages.voyages_dates`
+),
+all_combinations AS (
+  SELECT d.decade, r.broad_region
+  FROM distinct_decades d
+  CROSS JOIN (SELECT DISTINCT broad_region FROM labeled_regions) r
+),
+percent_complete AS (
+  SELECT
+    r.broad_region,
+    i.decade,
+    COUNT(s.slaves_embarked_total) / COUNT(*) * 100 AS completeness_pct
+  FROM `slave_voyages.voyages_slave_numbers` s
+  JOIN disembark_regions i ON s.voyage_id = i.voyage_id
+  JOIN labeled_regions r ON i.broad_region_code = r.broad_region_value
+  GROUP BY r.broad_region, i.decade
+),
+filled_data AS (
+  SELECT
+    a.decade,
+    a.broad_region,
+    IFNULL(p.completeness_pct, 0) AS completeness_pct
+  FROM all_combinations a
+  LEFT JOIN percent_complete p ON a.decade = p.decade AND a.broad_region = p.broad_region
+)
+SELECT * FROM filled_data
+PIVOT (
+  MAX(completeness_pct)
+  FOR broad_region IN (
+    'Europe',
+    'Mainland North America',
+    'Caribbean',
+    'Spanish Mainland Americas',
+    'Brazil',
+    'Africa',
+    'Middle East',
+    'Asia',
+    'Other'
+  )
+)
+ORDER BY decade;
+
+-- Total Disembarkation Completeness by Decade with Broad Regions as Columns
+WITH disembark_regions AS (
+  SELECT
+    i.voyage_id,
+    FLOOR(d.voyage_start_imputed_year / 10) * 10 AS decade,
+    CAST(SUBSTR(CAST(i.primary_disembark_port AS STRING), 1, 1) AS INT64) * 10000 AS broad_region_code
+  FROM `slave_voyages.voyages_itinerary` i
+  JOIN `slave_voyages.voyages_dates` d ON i.voyage_id = d.voyage_id
+  WHERE i.primary_disembark_port IS NOT NULL
+),
+labeled_regions AS (
+  SELECT DISTINCT broad_region_value, broad_region
+  FROM `slave_voyages.lookup_voyages_geographic_codes`
+),
+distinct_decades AS (
+  SELECT DISTINCT FLOOR(voyage_start_imputed_year / 10) * 10 AS decade
+  FROM `slave_voyages.voyages_dates`
+),
+all_combinations AS (
+  SELECT d.decade, r.broad_region
+  FROM distinct_decades d
+  CROSS JOIN (SELECT DISTINCT broad_region FROM labeled_regions) r
+),
+percent_complete AS (
+  SELECT
+    r.broad_region,
+    i.decade,
+    COUNT(s.slaves_disembarked_total) / COUNT(*) * 100 AS completeness_pct
+  FROM `slave_voyages.voyages_slave_numbers` s
+  JOIN disembark_regions i ON s.voyage_id = i.voyage_id
+  JOIN labeled_regions r ON i.broad_region_code = r.broad_region_value
+  GROUP BY r.broad_region, i.decade
+),
+filled_data AS (
+  SELECT
+    a.decade,
+    a.broad_region,
+    IFNULL(p.completeness_pct, 0) AS completeness_pct
+  FROM all_combinations a
+  LEFT JOIN percent_complete p ON a.decade = p.decade AND a.broad_region = p.broad_region
+)
+SELECT * FROM filled_data
+PIVOT (
+  MAX(completeness_pct)
+  FOR broad_region IN (
+    'Europe',
+    'Mainland North America',
+    'Caribbean',
+    'Spanish Mainland Americas',
+    'Brazil',
+    'Africa',
+    'Middle East',
+    'Asia',
+    'Other'
+  )
+)
+ORDER BY decade;
+
+-- Total Deaths Completeness by Decade with Broad Regions as Columns
+WITH disembark_regions AS (
+  SELECT
+    i.voyage_id,
+    FLOOR(d.voyage_start_imputed_year / 10) * 10 AS decade,
+    CAST(SUBSTR(CAST(i.primary_disembark_port AS STRING), 1, 1) AS INT64) * 10000 AS broad_region_code
+  FROM `slave_voyages.voyages_itinerary` i
+  JOIN `slave_voyages.voyages_dates` d ON i.voyage_id = d.voyage_id
+  WHERE i.primary_disembark_port IS NOT NULL
+),
+labeled_regions AS (
+  SELECT DISTINCT broad_region_value, broad_region
+  FROM `slave_voyages.lookup_voyages_geographic_codes`
+),
+distinct_decades AS (
+  SELECT DISTINCT FLOOR(voyage_start_imputed_year / 10) * 10 AS decade
+  FROM `slave_voyages.voyages_dates`
+),
+all_combinations AS (
+  SELECT d.decade, r.broad_region
+  FROM distinct_decades d
+  CROSS JOIN (SELECT DISTINCT broad_region FROM labeled_regions) r
+),
+percent_complete AS (
+  SELECT
+    r.broad_region,
+    i.decade,
+    COUNT(c.slave_death_total_derived) / COUNT(*) * 100 AS completeness_pct
+  FROM `slave_voyages.voyages_slave_characteristics` c
+  JOIN disembark_regions i ON c.voyage_id = i.voyage_id
+  JOIN labeled_regions r ON i.broad_region_code = r.broad_region_value
+  GROUP BY r.broad_region, i.decade
+),
+filled_data AS (
+  SELECT
+    a.decade,
+    a.broad_region,
+    IFNULL(p.completeness_pct, 0) AS completeness_pct
+  FROM all_combinations a
+  LEFT JOIN percent_complete p ON a.decade = p.decade AND a.broad_region = p.broad_region
+)
+SELECT * FROM filled_data
+PIVOT (
+  MAX(completeness_pct)
+  FOR broad_region IN (
+    'Europe',
+    'Mainland North America',
+    'Caribbean',
+    'Spanish Mainland Americas',
+    'Brazil',
+    'Africa',
+    'Middle East',
+    'Asia',
+    'Other'
+  )
+)
+ORDER BY decade;
+
+-- Female Embarkation Completeness by Decade with Broad Regions as Columns
+WITH disembark_regions AS (
+  SELECT
+    i.voyage_id,
+    FLOOR(d.voyage_start_imputed_year / 10) * 10 AS decade,
+    CAST(SUBSTR(CAST(i.primary_disembark_port AS STRING), 1, 1) AS INT64) * 10000 AS broad_region_code
+  FROM `slave_voyages.voyages_itinerary` i
+  JOIN `slave_voyages.voyages_dates` d ON i.voyage_id = d.voyage_id
+  WHERE i.primary_disembark_port IS NOT NULL
+),
+labeled_regions AS (
+  SELECT DISTINCT broad_region_value, broad_region
+  FROM `slave_voyages.lookup_voyages_geographic_codes`
+),
+distinct_decades AS (
+  SELECT DISTINCT FLOOR(voyage_start_imputed_year / 10) * 10 AS decade
+  FROM `slave_voyages.voyages_dates`
+),
+all_combinations AS (
+  SELECT d.decade, r.broad_region
+  FROM distinct_decades d
+  CROSS JOIN (SELECT DISTINCT broad_region FROM labeled_regions) r
+),
+percent_complete AS (
+  SELECT
+    r.broad_region,
+    i.decade,
+    COUNT(c.female_embark_total) / COUNT(*) * 100 AS completeness_pct
+  FROM `slave_voyages.voyages_slave_characteristics` c
+  JOIN disembark_regions i ON c.voyage_id = i.voyage_id
+  JOIN labeled_regions r ON i.broad_region_code = r.broad_region_value
+  GROUP BY r.broad_region, i.decade
+),
+filled_data AS (
+  SELECT
+    a.decade,
+    a.broad_region,
+    IFNULL(p.completeness_pct, 0) AS completeness_pct
+  FROM all_combinations a
+  LEFT JOIN percent_complete p ON a.decade = p.decade AND a.broad_region = p.broad_region
+)
+SELECT * FROM filled_data
+PIVOT (
+  MAX(completeness_pct)
+  FOR broad_region IN (
+    'Europe',
+    'Mainland North America',
+    'Caribbean',
+    'Spanish Mainland Americas',
+    'Brazil',
+    'Africa',
+    'Middle East',
+    'Asia',
+    'Other'
+  )
+)
+ORDER BY decade;
+
+-- Female Disembarkation Completeness by Decade with Broad Regions as Columns
+WITH disembark_regions AS (
+  SELECT
+    i.voyage_id,
+    FLOOR(d.voyage_start_imputed_year / 10) * 10 AS decade,
+    CAST(SUBSTR(CAST(i.primary_disembark_port AS STRING), 1, 1) AS INT64) * 10000 AS broad_region_code
+  FROM `slave_voyages.voyages_itinerary` i
+  JOIN `slave_voyages.voyages_dates` d ON i.voyage_id = d.voyage_id
+  WHERE i.primary_disembark_port IS NOT NULL
+),
+labeled_regions AS (
+  SELECT DISTINCT broad_region_value, broad_region
+  FROM `slave_voyages.lookup_voyages_geographic_codes`
+),
+distinct_decades AS (
+  SELECT DISTINCT FLOOR(voyage_start_imputed_year / 10) * 10 AS decade
+  FROM `slave_voyages.voyages_dates`
+),
+all_combinations AS (
+  SELECT d.decade, r.broad_region
+  FROM distinct_decades d
+  CROSS JOIN (SELECT DISTINCT broad_region FROM labeled_regions) r
+),
+percent_complete AS (
+  SELECT
+    r.broad_region,
+    i.decade,
+    COUNT(c.female_arrival_total) / COUNT(*) * 100 AS completeness_pct
+  FROM `slave_voyages.voyages_slave_characteristics` c
+  JOIN disembark_regions i ON c.voyage_id = i.voyage_id
+  JOIN labeled_regions r ON i.broad_region_code = r.broad_region_value
+  GROUP BY r.broad_region, i.decade
+),
+filled_data AS (
+  SELECT
+    a.decade,
+    a.broad_region,
+    IFNULL(p.completeness_pct, 0) AS completeness_pct
+  FROM all_combinations a
+  LEFT JOIN percent_complete p ON a.decade = p.decade AND a.broad_region = p.broad_region
+)
+SELECT * FROM filled_data
+PIVOT (
+  MAX(completeness_pct)
+  FOR broad_region IN (
+    'Europe',
+    'Mainland North America',
+    'Caribbean',
+    'Spanish Mainland Americas',
+    'Brazil',
+    'Africa',
+    'Middle East',
+    'Asia',
+    'Other'
+  )
+)
+ORDER BY decade;
+
+-- Female Deaths Completeness by Decade with Broad Regions as Columns
+WITH disembark_regions AS (
+  SELECT
+    i.voyage_id,
+    FLOOR(d.voyage_start_imputed_year / 10) * 10 AS decade,
+    CAST(SUBSTR(CAST(i.primary_disembark_port AS STRING), 1, 1) AS INT64) * 10000 AS broad_region_code
+  FROM `slave_voyages.voyages_itinerary` i
+  JOIN `slave_voyages.voyages_dates` d ON i.voyage_id = d.voyage_id
+  WHERE i.primary_disembark_port IS NOT NULL
+),
+labeled_regions AS (
+  SELECT DISTINCT broad_region_value, broad_region
+  FROM `slave_voyages.lookup_voyages_geographic_codes`
+),
+distinct_decades AS (
+  SELECT DISTINCT FLOOR(voyage_start_imputed_year / 10) * 10 AS decade
+  FROM `slave_voyages.voyages_dates`
+),
+all_combinations AS (
+  SELECT d.decade, r.broad_region
+  FROM distinct_decades d
+  CROSS JOIN (SELECT DISTINCT broad_region FROM labeled_regions) r
+),
+percent_complete AS (
+  SELECT
+    r.broad_region,
+    i.decade,
+    COUNT(c.female_death_passage) / COUNT(*) * 100 AS completeness_pct
+  FROM `slave_voyages.voyages_slave_characteristics` c
+  JOIN disembark_regions i ON c.voyage_id = i.voyage_id
+  JOIN labeled_regions r ON i.broad_region_code = r.broad_region_value
+  GROUP BY r.broad_region, i.decade
+),
+filled_data AS (
+  SELECT
+    a.decade,
+    a.broad_region,
+    IFNULL(p.completeness_pct, 0) AS completeness_pct
+  FROM all_combinations a
+  LEFT JOIN percent_complete p ON a.decade = p.decade AND a.broad_region = p.broad_region
+)
+SELECT * FROM filled_data
+PIVOT (
+  MAX(completeness_pct)
+  FOR broad_region IN (
+    'Europe',
+    'Mainland North America',
+    'Caribbean',
+    'Spanish Mainland Americas',
+    'Brazil',
+    'Africa',
+    'Middle East',
+    'Asia',
+    'Other'
+  )
+)
+ORDER BY decade;
+
+-- Girl Embarkation Completeness by Decade with Broad Regions as Columns
+WITH disembark_regions AS (
+  SELECT
+    i.voyage_id,
+    FLOOR(d.voyage_start_imputed_year / 10) * 10 AS decade,
+    CAST(SUBSTR(CAST(i.primary_disembark_port AS STRING), 1, 1) AS INT64) * 10000 AS broad_region_code
+  FROM `slave_voyages.voyages_itinerary` i
+  JOIN `slave_voyages.voyages_dates` d ON i.voyage_id = d.voyage_id
+  WHERE i.primary_disembark_port IS NOT NULL
+),
+labeled_regions AS (
+  SELECT DISTINCT broad_region_value, broad_region
+  FROM `slave_voyages.lookup_voyages_geographic_codes`
+),
+distinct_decades AS (
+  SELECT DISTINCT FLOOR(voyage_start_imputed_year / 10) * 10 AS decade
+  FROM `slave_voyages.voyages_dates`
+),
+all_combinations AS (
+  SELECT d.decade, r.broad_region
+  FROM distinct_decades d
+  CROSS JOIN (SELECT DISTINCT broad_region FROM labeled_regions) r
+),
+percent_complete AS (
+  SELECT
+    r.broad_region,
+    i.decade,
+    COUNT(c.girl_embark_port1) / COUNT(*) * 100 AS completeness_pct
+  FROM `slave_voyages.voyages_slave_characteristics` c
+  JOIN disembark_regions i ON c.voyage_id = i.voyage_id
+  JOIN labeled_regions r ON i.broad_region_code = r.broad_region_value
+  GROUP BY r.broad_region, i.decade
+),
+filled_data AS (
+  SELECT
+    a.decade,
+    a.broad_region,
+    IFNULL(p.completeness_pct, 0) AS completeness_pct
+  FROM all_combinations a
+  LEFT JOIN percent_complete p ON a.decade = p.decade AND a.broad_region = p.broad_region
+)
+SELECT * FROM filled_data
+PIVOT (
+  MAX(completeness_pct)
+  FOR broad_region IN (
+    'Europe',
+    'Mainland North America',
+    'Caribbean',
+    'Spanish Mainland Americas',
+    'Brazil',
+    'Africa',
+    'Middle East',
+    'Asia',
+    'Other'
+  )
+)
+ORDER BY decade;
+
+-- Girl Disembarkation Completeness by Decade with Broad Regions as Columns
+WITH disembark_regions AS (
+  SELECT
+    i.voyage_id,
+    FLOOR(d.voyage_start_imputed_year / 10) * 10 AS decade,
+    CAST(SUBSTR(CAST(i.primary_disembark_port AS STRING), 1, 1) AS INT64) * 10000 AS broad_region_code
+  FROM `slave_voyages.voyages_itinerary` i
+  JOIN `slave_voyages.voyages_dates` d ON i.voyage_id = d.voyage_id
+  WHERE i.primary_disembark_port IS NOT NULL
+),
+labeled_regions AS (
+  SELECT DISTINCT broad_region_value, broad_region
+  FROM `slave_voyages.lookup_voyages_geographic_codes`
+),
+distinct_decades AS (
+  SELECT DISTINCT FLOOR(voyage_start_imputed_year / 10) * 10 AS decade
+  FROM `slave_voyages.voyages_dates`
+),
+all_combinations AS (
+  SELECT d.decade, r.broad_region
+  FROM distinct_decades d
+  CROSS JOIN (SELECT DISTINCT broad_region FROM labeled_regions) r
+),
+percent_complete AS (
+  SELECT
+    r.broad_region,
+    i.decade,
+    COUNT(c.girl_arrival_total) / COUNT(*) * 100 AS completeness_pct
+  FROM `slave_voyages.voyages_slave_characteristics` c
+  JOIN disembark_regions i ON c.voyage_id = i.voyage_id
+  JOIN labeled_regions r ON i.broad_region_code = r.broad_region_value
+  GROUP BY r.broad_region, i.decade
+),
+filled_data AS (
+  SELECT
+    a.decade,
+    a.broad_region,
+    IFNULL(p.completeness_pct, 0) AS completeness_pct
+  FROM all_combinations a
+  LEFT JOIN percent_complete p ON a.decade = p.decade AND a.broad_region = p.broad_region
+)
+SELECT * FROM filled_data
+PIVOT (
+  MAX(completeness_pct)
+  FOR broad_region IN (
+    'Europe',
+    'Mainland North America',
+    'Caribbean',
+    'Spanish Mainland Americas',
+    'Brazil',
+    'Africa',
+    'Middle East',
+    'Asia',
+    'Other'
+  )
+)
+ORDER BY decade;
+
+-- Girl Deaths Completeness by Decade with Broad Regions as Columns
+WITH disembark_regions AS (
+  SELECT
+    i.voyage_id,
+    FLOOR(d.voyage_start_imputed_year / 10) * 10 AS decade,
+    CAST(SUBSTR(CAST(i.primary_disembark_port AS STRING), 1, 1) AS INT64) * 10000 AS broad_region_code
+  FROM `slave_voyages.voyages_itinerary` i
+  JOIN `slave_voyages.voyages_dates` d ON i.voyage_id = d.voyage_id
+  WHERE i.primary_disembark_port IS NOT NULL
+),
+labeled_regions AS (
+  SELECT DISTINCT broad_region_value, broad_region
+  FROM `slave_voyages.lookup_voyages_geographic_codes`
+),
+distinct_decades AS (
+  SELECT DISTINCT FLOOR(voyage_start_imputed_year / 10) * 10 AS decade
+  FROM `slave_voyages.voyages_dates`
+),
+all_combinations AS (
+  SELECT d.decade, r.broad_region
+  FROM distinct_decades d
+  CROSS JOIN (SELECT DISTINCT broad_region FROM labeled_regions) r
+),
+percent_complete AS (
+  SELECT
+    r.broad_region,
+    i.decade,
+    COUNT(c.girl_died_passage) / COUNT(*) * 100 AS completeness_pct
+  FROM `slave_voyages.voyages_slave_characteristics` c
+  JOIN disembark_regions i ON c.voyage_id = i.voyage_id
+  JOIN labeled_regions r ON i.broad_region_code = r.broad_region_value
+  GROUP BY r.broad_region, i.decade
+),
+filled_data AS (
+  SELECT
+    a.decade,
+    a.broad_region,
+    IFNULL(p.completeness_pct, 0) AS completeness_pct
+  FROM all_combinations a
+  LEFT JOIN percent_complete p ON a.decade = p.decade AND a.broad_region = p.broad_region
+)
+SELECT * FROM filled_data
+PIVOT (
+  MAX(completeness_pct)
+  FOR broad_region IN (
+    'Europe',
+    'Mainland North America',
+    'Caribbean',
+    'Spanish Mainland Americas',
+    'Brazil',
+    'Africa',
+    'Middle East',
+    'Asia',
+    'Other'
+  )
+)
+ORDER BY decade;
